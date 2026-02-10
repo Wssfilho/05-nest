a1870a4e605e32658a993f7e7c206ad1be87a3aa
Wssfilho <wilsonsa004@gmail.com>
2026-02-10 16:21:25 -0300

    feat: atualizar configuração de testes para usar Vitest e adicionar arquivos de configuração e2e

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   test: {
+    include: ['**/*.e2e-spec.ts'],
     globals: true,
     root: './',
   },
