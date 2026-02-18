c6d97554e37701f6ae9cf8d284d67600aac8806a
Wssfilho <wilsonsa004@gmail.com>
2026-02-18 17:23:23 -0300

    feat: adicionar testes E2E para a criação de sessões no controller de autenticação

---
+++ src/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { AppModule } from '@/app.module';
+import { PrismaService } from '@/prisma/prisma.service';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { hash } from 'bcryptjs';
+import request from 'supertest';
+describe('authenticate accountController (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prisma = moduleRef.get<PrismaService>(PrismaService);
+    await app.init();
+  });
+  test('[POST] /sessions - should create a new session', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: await hash('123456', 8),
+      },
+    });
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      access_token: expect.any(String),
+    });
+  });
+});
