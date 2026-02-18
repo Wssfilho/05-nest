9e9a91d95102fe08d45717c3907783d78896e987
Wssfilho <wilsonsa004@gmail.com>
2026-02-18 17:23:30 -0300

    feat: adicionar verificação de usuário no teste E2E de criação de conta

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,16 +1,18 @@
 import { AppModule } from '@/app.module';
+import { PrismaService } from '@/prisma/prisma.service';
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import request from 'supertest';
 describe('create accountController (E2E)', () => {
   let app: INestApplication;
-
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleRef.createNestApplication();
+    prisma = moduleRef.get<PrismaService>(PrismaService);
     await app.init();
   });
   test('[POST] /accounts - should create a new account', async () => {
@@ -20,5 +22,12 @@ describe('create accountController (E2E)', () => {
       password: '123456',
     });
     expect(response.status).toBe(201);
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'johndoe@example.com',
+      },
+    });
+    expect(userOnDatabase).toBeTruthy();
   });
 });
