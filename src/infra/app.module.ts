7b15095a6923f8c79c82493ed17b9aadca8a6d8b
Wssfilho <wilsonsa004@gmail.com>
2026-02-19 16:18:24 -0300

    feat: adicionar módulo Http e reorganizar controllers

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
-import { createAccountController } from './http/controllers/create-account.controller';
 import { envSchema } from '@/infra/env';
 import { AuthModule } from './auth/auth.module';
-import { AuthenticateController } from './http/controllers/authenticate.controller';
-import { CreateQuestionController } from './http/controllers/create-question.controller';
-import { FetchRecentQuestionsController } from './http/controllers/fetch-recent-questions.controller';
+import { HttpModule } from './http/http.module';
 
 @Module({
   imports: [
@@ -17,13 +14,7 @@ import { FetchRecentQuestionsController } from './http/controllers/fetch-recent-
       isGlobal: true,
     }),
     AuthModule,
-  ],
-
-  controllers: [
-    createAccountController,
-    AuthenticateController,
-    CreateQuestionController,
-    FetchRecentQuestionsController,
+    HttpModule,
   ],
   providers: [PrismaService],
 })
---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AuthenticateController } from './controllers/authenticate.controller';
+import { createAccountController } from './controllers/create-account.controller';
+import { CreateQuestionController } from './controllers/create-question.controller';
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Module({
+  controllers: [
+    createAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+    FetchRecentQuestionsController,
+  ],
+  providers: [PrismaService],
+})
+export class HttpModule {}
