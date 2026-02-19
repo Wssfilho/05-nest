11c64e3a32d62d36b423cb037430fe509ec821c5
Wssfilho <wilsonsa004@gmail.com>
2026-02-19 16:08:41 -0300

    refactor: restructure application to use new directory structure and update environment handling
    - Moved environment configuration to src/infra/env.ts and updated references.
    - Refactored application module to src/infra/app.module.ts.
    - Updated authentication module to src/infra/auth/auth.module.ts.
    - Refactored controllers and services to new paths under src/infra/http/controllers and src/infra/prisma.
    - Replaced Zod validation pipe implementation and updated its path.
    - Removed old files and references to maintain clean codebase.

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Env } from 'env';
+import { Env } from '@/infra/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
